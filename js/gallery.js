30675a6bb1a47a0560578f06a3c73661d1f8f64e
tomagent <tommymora2003@gmail.com>
2021-05-04 22:22:45 -0500

    page completed

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,6 @@
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
 function activateGallery(){
   let thumbnails = document.querySelectorAll(" #gallery-thumbs > div > img");
   let mainImage = document.querySelector("#gallery-photo > img");
@@ -7,11 +10,19 @@ function activateGallery(){
       // Set clicked image as main image.
       let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", thumbnail.alt);
 
       // Change which image is current
-      let currentClass = "current";
-      document.querySelector(".current").classList.remove(currentClass);
-      thumbnail.parentNode.classList.add(currentClass);
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
+
+      // Update image info
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML        = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
     });
   });
 }
