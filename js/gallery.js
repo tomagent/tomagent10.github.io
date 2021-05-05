19a204af77ac559d96cc9cacf8b2df81f7c57e6c
tomagent <tommymora2003@gmail.com>
2021-05-04 21:37:24 -0500

    change image gallery javascript

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,12 @@
 function activateGallery(){
-  alert("hello from the gallery file!");
+  let thumbnails = document.querySelectorAll(" #gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo > img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc)
+    });
+  });
 }
